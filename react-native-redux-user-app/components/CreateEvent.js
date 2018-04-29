e5b9301e9d00b13bf6ec6f90bdd0de17e5fb37e7
bipinswarnkar1989 <vipinswarnkar1989@gmail.com>
2018-04-29 14:23:28 +0530

    Started buillding Event Form

--- react-native-redux-user-app/components/CreateEvent.js
+++ react-native-redux-user-app/components/CreateEvent.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet  } from 'react-native';
+import { 
+    View, 
+    Text,
+    StyleSheet,
+    TextInput,
+     } from 'react-native';
 import {
     Card,
     Container,
   Content,
-  Icon
+  Icon,
+  Button 
 } from 'native-base';
 import Loading from './Loading';
 import AppHeader from './AppHeader';
 
 class CreateEvent extends Component {
+ constructor(props){
+     super(props);
+     this.state = {
+         name:null,
+         inputHeight:0,
+         imageSelectOptions:false
+     }
+     this.toggleImageSelect = this.toggleImageSelect.bind(this);
+ }
+
+ toggleImageSelect(){
+    this.setState((prevState,props) => {
+        return {
+            imageSelectOptions:!prevState.imageSelectOptions
+        }
+    })
+}
   render() {
     return (
         <Container>
@@ -19,7 +42,44 @@ class CreateEvent extends Component {
        drawerOpen={() => this.props.navigation.navigate('DrawerOpen')}
        />
       <View style={styles.container}>
-      <Text>CreateEvent</Text>
+      <View style={styles.formContainer}>
+      <TextInput 
+       autoCapitalize="none"
+       style={styles.inputStyle}
+       onChangeText={value => this.setState({name:value})}
+       placeholder="Event Name"
+       placeholderTextColor="#DE005E"
+       />
+       <TextInput
+        autoCapitalize="none"
+        multiline={true}
+        onChangeText={value => this.setState({description:value})}
+        placeholder="Description"
+        placeholderTextColor="#DE005E"
+        style={[styles.inputStyle, {height:Math.max(35, this.state.inputHeight)}]}
+        onContentSizeChange={(event) => {
+            this.setState({ inputHeight: event.nativeEvent.contentSize.height+30  })
+        }}
+        />
+        <View style={styles.imageContainer}>
+        {this.state.imageSelectOptions && 
+           <View style={styles.imageSelectOptions}>
+           <View style={{alignItems:'center'}}>
+           <Icon name='camera' />
+           <Text>Camera</Text>
+           </View>
+           <View style={{alignItems:'center'}}>
+           <Icon name='images' />
+           <Text>Gallery</Text>
+           </View>
+         </View>
+        }
+        <Button full iconLeft onPress={this.toggleImageSelect}>
+            <Icon name='image' />
+            <Text style={{paddingLeft:10, color:'white'}}>Event Image</Text>
+          </Button>
+        </View>
+      </View>
       </View>
       </Content>
       </Container>
@@ -30,6 +90,28 @@ class CreateEvent extends Component {
 const styles = StyleSheet.create({
     container:{
         flex: 1,
+        alignItems: 'center',
+    },
+    formContainer:{
+      flexDirection: 'column',
+      alignSelf: 'stretch',
+      padding: 20,
+    },
+    inputStyle:{
+        minHeight:60,
+        fontSize: 15,
+        padding:3
+    },
+    imageContainer:{
+        zIndex:1,
+        alignItems: 'center',
+    },
+    imageSelectOptions:{
+       flex:1,
+       flexDirection: 'row',
+       justifyContent:'space-between',
+       width:200,
+       
     }
 })
 
